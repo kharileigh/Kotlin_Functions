2035da6463d3654c43ca354e594d3a6c2ec7f585
kharileigh <kharileighmiller@gmail.com>
2024-10-31 15:42:54 +0000

    Conditionals

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,8 @@ package org.example
 fun main() {
 
     /*
-    * 1. Write a method called `superify`
+    * [FUNCTIONS]
+    * Write a method called `superify`
     - arg : a String
     - adds word “super” to the start of String
     - return : String
@@ -15,7 +16,8 @@ fun main() {
 
 
     /*
-    * 2. Define a function `revealSecret`
+    * [FUNCTIONS]
+    * Define a function `revealSecret`
     - arg : passcode
     - returns a secret String : only if passcode is “HUNGRY”
     - returns null if not given passcode
@@ -34,4 +36,44 @@ fun main() {
     // incorrect passcode
     val incorrectSecret = revealSecret("FULL")
     println(incorrectSecret)
+
+
+    /*
+    * [CONDITIONAL]
+    * Define a function `getNumberSign`
+    - arg : a number
+    - returns : String → “zero” / “positive” / “negative”
+    - returns null if not a number
+    * */
+    fun getNumberSign(number: Any): String? {
+        return when (number) {
+           is Number -> when {
+               number.toDouble() == 0.0 -> "zero"
+               number.toDouble() > 0.0 -> "positive"
+               else -> "negative"
+           }
+            else -> null
+        }
+    }
+    println(getNumberSign(3))
+    println(getNumberSign(0))
+    println(getNumberSign(-5))
+    println(getNumberSign("is not a number"))
+
+    /*
+    * [DATA STRUCTURES]
+    * Define a function `printHelloMessages`
+    - arg : list of names
+    - prints a hello message
+    - use `map` function
+    * */
+
+
+    /*
+    * [DATA STRUCTURES]
+    * Use function `getNumberSign` (conditionals), create a new function `getAllNumberSigns`
+    - arg : array of numbers (Int)
+    - returns array of corresponding number signs
+    - use `map` function
+    * */
 }
